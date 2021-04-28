d764358e63e4bdfec9272d924f36dbce76adb2c0
Rahul Kashyap <rahulkashyap4041@gmail.com>
2021-04-28 10:45:57 +0530

    Converting Integers

--- practice/loops/ConvertingIntegers.java
+++ practice/loops/ConvertingIntegers.java
@@ -1,2 +1,23 @@
-package practice.loops;public class ConvertingIntegers {
+package practice.loops;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ConvertingIntegers {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int givenBase = Integer.parseInt(br.readLine());
+        long num = Long.parseLong(br.readLine());
+        int toBase = Integer.parseInt(br.readLine());
+        long res = 0;
+        int i = 0;
+        while (num != 0) {
+            long rem = num % toBase;
+            res += (long) (rem * Math.pow(givenBase, i++));
+            num /= toBase;
+        }
+        System.out.println(res);
+        br.close();
+    }
 }
