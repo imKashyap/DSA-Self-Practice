46183b42b986388a76f6e0e72a6b1a62cbbc9fd6
Rahul Kashyap <rahulkashyap4041@gmail.com>
2021-04-30 08:19:43 +0530

    Made Node class public

--- practice/linkedlist/MyLinkedList.java
+++ practice/linkedlist/MyLinkedList.java
@@ -1,97 +1,97 @@
 package practice.linkedlist;
 
 public class MyLinkedList<E> {
-	public Node<E> head;
+    public Node<E> head;
 
-	public void add(E data) {
-		Node<E> toAdd;
-		toAdd = new Node<>(data);
-		if (head == null) {
-			head = toAdd;
-			return;
-		}
-		Node<E> temp = head;
-		while (temp.next != null) {
-			temp = temp.next;
-		}
-		temp.next = toAdd;
-	}
+    public void add(E data) {
+        Node<E> toAdd;
+        toAdd = new Node<>(data);
+        if (head == null) {
+            head = toAdd;
+            return;
+        }
+        Node<E> temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = toAdd;
+    }
 
-	public void remove(int index) throws Exception {
-		if (index > size() - 1)
-			throw new Exception("Index must be smaller than size of linked list.");
-		if (head == null)
-			throw new Exception("Cannot remove from an empty Linked List");
-		if (index < 0)
-			throw new Exception("Index must be greater than or equal to zero.");
-		if (head.next == null) {
-			head = null;
-			return;
-		}
-		if (index == 0) {
-			head = head.next;
-			return;
-		}
-		Node<E> temp = head;
-		int i = 0;
-		while (temp.next != null) {
-			if (i + 1 == index) {
-				if (temp.next.next == null)
-					temp.next = null;
-				else
-					temp.next = temp.next.next;
-				break;
+    public void remove(int index) throws Exception {
+        if (index > size() - 1)
+            throw new Exception("Index must be smaller than size of linked list.");
+        if (head == null)
+            throw new Exception("Cannot remove from an empty Linked List");
+        if (index < 0)
+            throw new Exception("Index must be greater than or equal to zero.");
+        if (head.next == null) {
+            head = null;
+            return;
+        }
+        if (index == 0) {
+            head = head.next;
+            return;
+        }
+        Node<E> temp = head;
+        int i = 0;
+        while (temp.next != null) {
+            if (i + 1 == index) {
+                if (temp.next.next == null)
+                    temp.next = null;
+                else
+                    temp.next = temp.next.next;
+                break;
 
-			}
-			temp = temp.next;
-			i++;
-		}
+            }
+            temp = temp.next;
+            i++;
+        }
 
-	}
+    }
 
-	public int size() {
-		if (head == null)
-			return 0;
-		int count = 1;
-		Node<E> temp = head;
-		while (temp.next != null) {
-			temp = temp.next;
-			count++;
-		}
-		return count;
-	}
+    public int size() {
+        if (head == null)
+            return 0;
+        int count = 1;
+        Node<E> temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+            count++;
+        }
+        return count;
+    }
 
-	public E get(int index) throws Exception {
-		if (head == null)
-			throw new Exception("Cannot peek from an empty Linked List");
-		if (index < 0)
-			throw new Exception("Index must be greater than or equal to zero.");
-		if (index > size() - 1)
-			throw new Exception("Index must be smaller than size of linked list.");
-		Node<E> temp = head;
-		int i = 0;
-		E data = null;
-		while (temp != null) {
-			if (i == index) {
-				data = temp.data;
-			}
-			temp = temp.next;
-			i++;
-		}
-		return data;
+    public E get(int index) throws Exception {
+        if (head == null)
+            throw new Exception("Cannot peek from an empty Linked List");
+        if (index < 0)
+            throw new Exception("Index must be greater than or equal to zero.");
+        if (index > size() - 1)
+            throw new Exception("Index must be smaller than size of linked list.");
+        Node<E> temp = head;
+        int i = 0;
+        E data = null;
+        while (temp != null) {
+            if (i == index) {
+                data = temp.data;
+            }
+            temp = temp.next;
+            i++;
+        }
+        return data;
 
-	}
+    }
 
-	public boolean isEmpty() {
-		return head == null;
-	}
+    public boolean isEmpty() {
+        return head == null;
+    }
 
-	public static class Node<E> {
-		E data;
-		Node<E> next;
+    public static class Node<E> {
+        public E data;
+        public Node<E> next;
 
-		Node(E data) {
-			this.data = data;
-		}
-	}
+        public Node(E data) {
+            this.data = data;
+        }
+    }
 }
