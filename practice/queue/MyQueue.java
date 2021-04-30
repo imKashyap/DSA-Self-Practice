8543068abbc6b0627f365daa593eb0235933f6c1
Rahul Kashyap <rahulkashyap4041@gmail.com>
2021-04-30 08:20:20 +0530

    Created My custom Queue

--- practice/queue/MyQueue.java
+++ practice/queue/MyQueue.java
@@ -1,5 +1,41 @@
 package practice.queue;
 
-public class MyQueue {
+import practice.linkedlist.MyLinkedList.Node;
+
+public class MyQueue<E> {
+    private Node<E> head, rear;
+
+    void enqueue(E e) {
+        Node<E> toAdd = new Node<>(e);
+        if (head == null)
+            head = rear = toAdd;
+        else {
+            rear.next = toAdd;
+            rear = rear.next;
+        }
+
+    }
+
+    E dequeue() throws Exception {
+        if (head == null) throw new Exception("Cannot remove from a Queue");
+        Node<E> temp = head;
+        head = head.next;
+        if (head == null) rear = null;
+        return temp.data;
+
+    }
+
+    public int size() {
+        if (head == null)
+            return 0;
+        int count = 1;
+        Node<E> temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+            count++;
+        }
+        return count;
+    }
+
 
 }
